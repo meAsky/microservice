d29552934a01b903ee0d5d8783b7b6b1dd43ae7c
liuxianghua <liuxianghua1014@rayootech.com>
2021-02-13 17:59:41 +0800

    发短信、邮件的消息服务

--- message-thrift-python-service/messsage/message_service.py
+++ message-thrift-python-service/messsage/message_service.py
@@ -1,9 +1,46 @@
+# coding: utf-8
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
 
+from messsage.api import MessageService
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+sender = 'imoocd@163.com'
+authcode = 'aA111111'
 class MessageServiceHandler:
     def sendMobileMessage(self, mobile, message):
-        print "sendMobileMessage"
+        print "sendMobileMessage, mobile:"+mobile+", message:"+message
         return True
 
     def sendEmailMessage(self, email, message):
-        print "sendEmailMessage"
-        return True
+        print "sendEmailMessage, email:"+email+" ,messge:"+message
+        messageObj = MIMEText(message, "plain","utf-8")
+        messageObj['From'] = sender
+        messageObj['To'] = email
+        messageObj['Subject'] = Header('容器化邮件','utf-8')
+        try:
+            smtpObj = smtplib.SMTP("smpt.163.com")
+            smtpObj.login(sender,authcode)
+            smtpObj.sendmail(sender,[email],messageObj.as_string())
+            print "send mail success"
+            return True
+        except smtplib.SMTPException, ex:
+            print "send mail failed!"
+            print ex
+            return False
+
+
+if __name__ == '__main__':
+    handler = MessageServiceHandler()
+    processor = MessageService.Processor(handler)
+    transport = TSocket.TServerSocket("localhost","9090")
+    tfactory = TTransport.TFramedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    server = TServer.TSimpleServer(processor, transport , tfactory, pfactory)
+    print "python thrift server start"
+    server.serve()
+    print "python thrift server exit"
--- message-thrift-python-service/thrift/gen-code.bat
+++ message-thrift-python-service/thrift/gen-code.bat
@@ -1,1 +1,8 @@
+#生成py代码
 thrift --gen py -out ../ message.thrift
+
+#生成java代码
+thrift --gen java -out ../../message-thrift-service-api/src/main/java message.thrift
+
+# 查看端口是否被占用
+netstat -aon|findstr "9090"
