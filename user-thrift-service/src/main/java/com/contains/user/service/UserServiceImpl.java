7c612a745e54ec4571c733b44956e46799b1a2c9
liuxianghua <liuxianghua1014@rayootech.com>
2021-02-14 12:44:40 +0800

    用户服务

---
+++ user-thrift-service/src/main/java/com/contains/user/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.contains.user.service;
+
+import com.contains.thrift.user.UserInfo;
+import com.contains.thrift.user.UserService;
+import com.contains.user.mapper.UserMapper;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @date: 2021-02-13 18:18
+ **/
+@Service
+public class UserServiceImpl implements UserService.Iface {
+    @Autowired
+    private UserMapper userMapper;
+
+    public UserInfo getUserById(int id) throws TException {
+        return userMapper.getUserById(id);
+    }
+
+    public UserInfo getUserByName(String username) throws TException {
+        return userMapper.getUserByName(username);
+    }
+
+    public void regiserUser(UserInfo userInfo) throws TException {
+        userMapper.registerUser(userInfo);
+    }
+}
